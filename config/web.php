ffce5f33ebcfc40a94d78a88651285b3dd320e90
RyosukeMurai <rmurai@pro-seeds.co.jp>
2015-08-25 03:12:34 +0900

    any

--- config/web.php
+++ config/web.php
@@ -57,7 +57,6 @@ if (YII_ENV_DEV) {
     $config['bootstrap'][] = 'debug';
     $config['modules']['debug'] = [
         'class' => 'yii\debug\Module',
-        'allowedIPs'=>'*',
     ];
 
     $config['bootstrap'][] = 'gii';
